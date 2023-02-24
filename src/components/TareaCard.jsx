04a5b042e279c0264b12084279d1a21657c20213
JoseAlvarezMartinez <alvarezmjose2021@gmail.com>
2023-02-24 16:04:00 -0300

    -- Tareas Check --

--- src/components/TareaCard.jsx
+++ src/components/TareaCard.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
-
-const TareaCard = ({tarea}) => {
+import IconCheck from "../assets/icon-check.svg";
+import IconCross from "../assets/icon-cross.svg";
+import { useState } from "react";
+const TareaCard = ({ tarea }) => {
+  const [tareaFinalizada, setTareaFinalizada] = useState(false);
   return (
-    <div className='tarea-card'>
-      <p>{tarea}</p>
+    <div className="tarea-card">
+      <div className="tarea-centrado">
+        <div
+          className={`${tareaFinalizada && "tarea-hecha"} circulo-tarea`}
+          onClick={() => setTareaFinalizada(!tareaFinalizada)}
+        >
+          {tareaFinalizada && (
+            <img
+              src={IconCheck}
+              className={"check-icon"}
+              color={"#000"}
+              alt=""
+            />
+          )}
+        </div>
+        <p className={`${tareaFinalizada && "tarea-hecha-parrafo"}`}>{tarea}</p>
+        <div className="linea"></div>
+      </div>
+      <img src={IconCross} className="cross-icon" alt="" />
     </div>
-  )
-}
+  );
+};
 
-export default TareaCard
+export default TareaCard;
